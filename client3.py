742df671d5f9ab93b98665eef9ab604fdcf73819
Abhishek Kumar <21je0028@iitism.ac.in>
2023-12-26 15:19:14 +0530

    fixed the file

--- client3.py
+++ client3.py
@@ -25,9 +25,8 @@ import urllib.request
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
 
-# 500 server request
+# 500 server requests
 N = 500
-
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
@@ -35,15 +34,15 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Corrected formula for stock price
     return stock, bid_price, ask_price, price
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
-
+    if price_b == 0:
+        return float('inf')  # Handle division by zero
+    return price_a / price_b
 
 # Main
 if __name__ == "__main__":
@@ -51,9 +50,15 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
+        prices = {}  # Dictionary to store stock prices
+
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price  # Store the stock price for later use
 
-        print("Ratio %s" % getRatio(price, price))
+        # Calculate and print the ratio for each stock
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            ratio = getRatio(price, prices[stock])
+            print("Quoted %s at (bid:%s, ask:%s, price:%s), Ratio: %s" % (stock, bid_price, ask_price, price, ratio))
